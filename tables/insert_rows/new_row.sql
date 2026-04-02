7c19b5221d78a7e7604d9fb1d76c09eae26a246a
Kowsor Dip <mddip2000@gmail.com>
2026-04-02 19:13:19 +0600

    inserting new rows

---
+++ tables/insert_rows/new_row.sql
@@ -0,0 +1,8 @@
+USE mydb;
+
+INSERT INTO employees
+VALUES(1, "Eugene", "Krabs", 25.50, "2023-01-02"),
+	  (2, "Squidward", "Tentacles", 15.00, "2023-01-03"), 
+      (3, "Spongebob", "Squarepants", 12.50, "2023-01-04"), 
+	  (4, "Patrick", "Star", 12.50, "2023-01-05"),
+      (5, "Sandy", "Cheeks", 17.50, "2023-01-06");
