be1ee232ac011612fd6ead56e886ba28d0b7a24b
Kowsor Dip <mddip2000@gmail.com>
2026-04-02 19:59:54 +0600

    added more examples to select_from_where

--- select/select_from_where.sql
+++ select/select_from_where.sql
@@ -1,3 +1,11 @@
 SELECT last_name, first_name -- use * to select all cols
 FROM employees
 WHERE employee_id = 2; -- shows the last_name and first_name of those employee_id is 2
+
+SELECT *
+FROM employees
+WHERE hourly_pay >= 15;
+
+SELECT *
+FROM employees
+WHERE WHERE employee_id!=1;
